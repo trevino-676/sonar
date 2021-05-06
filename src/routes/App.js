d231ac0fb498ed8a33813d93f24875bff33cd6b1
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-05-06 17:21:30 -0500

    Se agregaron las rutas de usuarios, configuracion y empresas

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Se importan en esta seccion la paginas de la aplicacion
-import Layout from '../components/Layout'; 
+import Layout from '../components/Layout';
 import Home from '../pages/Home';
-
+import Settings from '../pages/Settings';
+// import AppContext from '../context/AppContext';
 
 const App = () => (
-        <BrowserRouter>
-            <Layout>
-                <Switch>
-                    {/* Aqui van las rutas de la aplicacion */}
-                    <Route exact path="/" component={Home} />
-                </Switch>
-            </Layout>
-        </BrowserRouter>
-    )
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          {/* Aqui van las rutas de la aplicacion */}
+          <Route exact path="/" component={Home} />
+          <Route exact path="/users" component={Home} />
+          <Route exact path="/settings" component={Settings} />
+          <Route exact path="/companies" component={Home} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
 
 export default App;
