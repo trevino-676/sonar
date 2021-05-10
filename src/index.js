99e1fe36aeb0226d30aa710d8c3038064b8ca382
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-05-09 22:06:32 -0500

    Se creo el store del contexto de la aplicacion

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './routes/App';
+import store from './store';
 
-ReactDOM.render( <App />, document.getElementById('app'));
+ReactDOM.render( 
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('app'));
