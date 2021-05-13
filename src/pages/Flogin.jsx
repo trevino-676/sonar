92c43b9e7d1a59c3660d00030d0c3e650c836afe
JLB <juanlb1844@gmail.com>
2021-05-13 07:50:49 -0500

    mje

--- src/pages/Flogin.jsx
+++ src/pages/Flogin.jsx
@@ -4,8 +4,7 @@ import React from 'react';
 const Flogin = () => (
     <>
     <h1>FLOGIN</h1>
-   	 <iframe height="200" src="https://www.facebook.com/v4.0/dialog/oauth?response_type=token&display=popup&client_id=2174258689290336&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer%2Fcallback&auth_type=rerequest&scope=public_profile"/>
-    </> 
+     
 )
 
 export default Flogin; 
