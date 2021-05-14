fb20ef175c4f9b947e4c77523d0f0ae826343678
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-05-13 19:15:13 -0500

    Cambios Juan

---
+++ src/pages/Flogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Flogin = () => (
+  <>
+    <h1>FLOGIN</h1>
+  </>
+);
+
+export default Flogin;
--- src/routes/App.js
+++ src/routes/App.js
@@ -5,20 +5,31 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Home from '../pages/Home';
 import Settings from '../pages/Settings';
-// import AppContext from '../context/AppContext';
+import Registro from '../pages/Registro';
+import Landing from '../pages/Landing';
+import Login from '../pages/Login';
+
+import RegistroEmpresa from '../pages/RegistroEmpresa';
+
+import Flogin from '../pages/Flogin';
 
 const App = () => (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          {/* Aqui van las rutas de la aplicacion */}
-          <Route exact path="/" component={Home} />
-          <Route exact path="/users" component={Home} />
-          <Route exact path="/settings" component={Settings} />
-          <Route exact path="/companies" component={Home} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
-  );
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        {/* Aqui van las rutas de la aplicacion */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/users" component={Home} />
+        <Route exact path="/settings" component={Settings} />
+        <Route exact path="/companies" component={Home} />
+        <Route exact path="/registro" component={Registro} />
+        <Route exact path="/landing" component={Landing} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/Flogin" component={Flogin} />
+        <Route exact path="/RegistroEmpresa" component={RegistroEmpresa} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
+);
 
 export default App;
