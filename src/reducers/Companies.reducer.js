b955ebffde761c77463374adfaa1a04546ebeb66
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-05-20 19:22:28 -0500

    Se creo el reducer de companias y se agrego en el root reducer

---
+++ src/reducers/Companies.reducer.js
@@ -0,0 +1,53 @@
+import CompaniesConstants from '../constants/Companies.constants';
+
+const initialState = {
+  name: '',
+  address: '',
+  rfc: '',
+  users: [],
+};
+
+const CompanyReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CompaniesConstants.ADD_COMPANY_REQUEST:
+      return { ...state };
+    case CompaniesConstants.ADD_COMPANY_REQUEST_SUCCESS:
+      return {
+        ...state,
+        name: action.payload.name,
+        address: action.payload.address,
+        rfc: action.payload.rfc,
+        users: action.payload.users
+      };
+    case CompaniesConstants.ADD_COMPANY_REQUEST_FAIL:
+        return {...state, error: action.payload.error};
+    case CompaniesConstants.GET_COMPANIES_REQUEST:
+        return {...state};
+    case CompaniesConstants.GET_COMPANIES_REQUEST_SUCCESS:
+        return {...state, companies: action.payload};
+    case CompaniesConstants.GET_COMPANIES_REQUEST_FAIL:
+        return {...state, error: action.payload.error};
+    case CompaniesConstants.UPDATE_COMPANY_REQUEST:
+        return {...state};
+    case CompaniesConstants.UPDATE_COMPANY_REQUEST_SUCCES:
+        return {
+            ...state,
+            name: action.payload.name,
+            address: action.payload.address,
+            rfc: action.payload.rfc,
+            users: action.payload.users
+          };
+    case CompaniesConstants.UPDATE_COMPANY_REQUEST_FAIL:
+        return {...state, error: action.payload.error};
+    case CompaniesConstants.DELETE_COMPANY_REQUEST:
+        return {...state};
+    case CompaniesConstants.DELETE_COMPANY_REQUEST_SUCCESS:
+        return {...state, name: "", address: "", rfc: "", users:[]};
+    case CompaniesConstants.DELETE_COMPANY_REQUEST_FAIL:
+        return {...state, error: action.payload.error};
+    default:
+      return { ...state };
+  }
+};
+
+export default CompanyReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 
 import UserReducer from './user.reducer';
 import ModalReducer from './modal.reducer';
+import CompanyReducer from './Companies.reducer';
 
 
 const rootReducer = combineReducers({
     user: UserReducer,
     modal: ModalReducer,
+    companies: CompanyReducer,
 });
 
 export default rootReducer;
