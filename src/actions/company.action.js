933519d68d7b2907a565a209fb24c0c59be3e777
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-05-21 13:30:43 -0500

    Se crearon las acciones de la compania

---
+++ src/actions/company.action.js
@@ -0,0 +1,151 @@
+import CompaniesConstants from '../constants/Companies.constants';
+import CompanyService from '../service/company';
+import ModalActions from './modal.action';
+
+const createCompany = (company) => {
+  const request = () => ({ type: CompaniesConstants.ADD_COMPANY_REQUEST });
+  const success = (companySaved) => ({
+    type: CompaniesConstants.ADD_COMPANY_REQUEST_SUCCESS,
+    payload: companySaved,
+  });
+  const fail = (message) => ({
+    type: CompaniesConstants.ADD_COMPANY_REQUEST_FAIL,
+    payload: { error: message },
+  });
+
+  return async (dispatch) => {
+    try {
+      dispatch(request());
+      dispatch(ModalActions.Clean());
+      const resp = await CompanyService.createCompany(company);
+      if (resp.status !== 200) {
+        dispatch(fail(resp.data.message));
+        dispatch(
+          ModalActions.Error({
+            title: 'Error al crear la compania',
+            body: resp.data.message,
+          })
+        );
+      }
+      dispatch(success(company));
+      dispatch(
+        ModalActions.Success({
+          title: 'Compañia',
+          body: 'Se agrego con exito una nueva compañia',
+        })
+      );
+    } catch (e) {
+      dispatch(ModalActions.Clean());
+      dispatch(fail(e.message));
+      dispatch(
+        ModalActions.Error({
+          title: 'Error al crear la compania',
+          body: e.message,
+        })
+      );
+    }
+  };
+};
+
+const getCompanies = (type, filters) => {
+  const request = () => ({ type: CompaniesConstants.GET_COMPANIES_REQUEST });
+  const success = (companies) => ({
+    type: CompaniesConstants.GET_COMPANIES_REQUEST_SUCCESS,
+    payload: companies,
+  });
+  const fail = (error) => ({
+    type: CompaniesConstants.ADD_COMPANY_REQUEST_FAIL,
+    payload: { error },
+  });
+  return async (dispatch) => {
+    dispatch(request());
+    dispatch(ModalActions.Clean());
+    try {
+      const resp = await CompanyService.findCompanies(type, filters);
+      if (resp.status !== 200) {
+        dispatch(fail(resp.data.message));
+        dispatch(
+          ModalActions.Error({ header: 'Companias', body: resp.data.message })
+        );
+      }
+      dispatch(success(resp.data.companies));
+    } catch (e) {
+      dispatch(fail(e));
+      dispatch(ModalActions.Error({ header: 'Companias', body: e }));
+    }
+  };
+};
+
+const updateCompany = (company) => {
+  const request = () => ({ type: CompaniesConstants.UPDATE_COMPANY_REQUEST });
+  const success = (newCompany) => ({
+    type: CompaniesConstants.UPDATE_COMPANY_REQUEST,
+    payload: newCompany,
+  });
+  const fail = (error) => ({
+    type: CompaniesConstants.UPDATE_COMPANY_REQUEST_FAIL,
+    error,
+  });
+
+  return async (dispatch) => {
+    dispatch(request());
+    dispatch(ModalActions.Clean());
+    try {
+      const resp = await CompanyService.updateCompany(company);
+      if (resp.status !== 200) {
+        dispatch(fail(resp.data.message));
+        dispatch(
+          ModalActions.Error({ header: 'Compania', body: resp.data.message })
+        );
+      }
+      dispatch(success(company));
+      dispatch(
+        ModalActions.Success({ header: 'Compania', body: resp.data.message })
+      );
+    } catch (e) {
+      dispatch(fail(e));
+      dispatch(ModalActions.Error({ header: 'Compania', body: e }));
+    }
+  };
+};
+
+const deleteCompany = (id) => {
+  const request = () => ({ type: CompaniesConstants.DELETE_COMPANY_REQUEST });
+  const success = () => ({
+    type: CompaniesConstants.DELETE_COMPANY_REQUEST_SUCCESS,
+  });
+  const fail = (error) => ({
+    type: CompaniesConstants.DELETE_COMPANY_REQUEST_FAIL,
+    error,
+  });
+
+  return async (dispatch) => {
+    dispatch(request());
+    dispatch(ModalActions.Clean());
+    try {
+      const resp = await CompanyService.deleteCompany(id);
+      if (resp.status !== 200) {
+        dispatch(fail(resp.data.message));
+        dispatch(
+          ModalActions.Error({ header: 'Company', body: resp.data.message })
+        );
+      }
+      dispatch(success());
+      dispatch(
+        ModalActions.Success({ header: 'Company', body: resp.data.message })
+      );
+    } catch (e) {
+      dispatch(fail(e));
+      dispatch(ModalActions.Error({ header: 'Company', body: e }));
+    }
+  };
+};
+
+const CompanyActions = {
+  createCompany,
+  getCompanies,
+  updateCompany,
+  deleteCompany,
+};
+
+export default CompanyActions;
