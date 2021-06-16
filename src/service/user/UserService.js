c12cc76f6b83ff6dad435dfb6012a4c634b11adb
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-06-16 12:01:43 -0500

    Se creo el servicio de usuarios

---
+++ src/service/user/UserService.js
@@ -0,0 +1,75 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:5000';
+
+const getHeaders = (token) => ({
+  'Content-Type': 'application/json',
+  Authorization: `jwt ${token}`,
+});
+
+const GetUsers = async (type, filters) => {
+  const url = `${baseURL}/v1/user/all`;
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return 401;
+  }
+
+  const headers = getHeaders(token);
+  const params = { type, filters };
+  const resp = await axios.get(url, { headers, params });
+  return resp;
+};
+
+const UpdateUser = async (oldUser) => {
+  const url = `${baseURL}/v1/user`;
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return 401;
+  }
+
+  const headers = getHeaders(token);
+  try {
+    const resp = await axios.put(url, oldUser, { headers });
+    if (resp.status === 200) {
+      return resp.data.status;
+    }
+    return null;
+  } catch (e) {
+    if (e.message.indexOf('401') > 0) {
+      return 401;
+    }
+    return null;
+  }
+};
+
+const DeleteUser = async (id) => {
+  const url = `${baseURL}/v1/user`;
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return 401;
+  }
+  const headers = getHeaders(token);
+  try {
+    const resp = await axios.delete(url, { headers, params: { id } });
+    if (resp.status === 200) {
+      return resp.data.status;
+    }
+    return null;
+  } catch (e) {
+    if (e.message.indexOf('401') > 0) {
+      return 401;
+    }
+    return null;
+  }
+};
+
+const UserService = {
+  GetUsers,
+  UpdateUser,
+  DeleteUser,
+};
+
+export default UserService;
