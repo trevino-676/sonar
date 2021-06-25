7782670cc9b02ac11100b07e3349c2da687c4c7e
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-06-25 14:03:47 -0500

    Se agrego el reducer del reporte de ventas al root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,13 @@ import { combineReducers } from 'redux';
 import UserReducer from './user.reducer';
 import ModalReducer from './modal.reducer';
 import CompanyReducer from './Companies.reducer';
-
+import SellsReportsReducer from './SellsReport.reducer';
 
 const rootReducer = combineReducers({
-    user: UserReducer,
-    modal: ModalReducer,
-    companies: CompanyReducer,
+  user: UserReducer,
+  modal: ModalReducer,
+  companies: CompanyReducer,
+  sell_reports: SellsReportsReducer,
 });
 
 export default rootReducer;
