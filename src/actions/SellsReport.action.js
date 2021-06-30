f6b98d68a54c2a8e68330acb6b847a6dd2b1db73
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-06-29 23:51:49 -0500

    Se agrego la accion para el reporte de ventas por articulos

--- src/actions/SellsReport.action.js
+++ src/actions/SellsReport.action.js
@@ -36,8 +36,41 @@ const byClients = (filters) => {
   };
 };
 
+const byItems = (filters) => {
+  const failMessage = 'Hubo un error en la peticion del reporte';
+  const request = () => ({
+    type: SellsReportsConstants.GET_SELL_REPORT_BY_ITEMS_REQUEST,
+  });
+  const success = (data) => ({
+    type: SellsReportsConstants.GET_SELL_REPORT_BY_ITEMS_SUCCESS,
+    payload: data,
+  });
+  const fail = (error) => ({
+    type: SellsReportsConstants.GET_SELL_REPORT_BY_ITEMS_FAIL,
+    payload: { error },
+  });
+  return async (dispatch) => {
+    dispatch(request());
+    dispatch(ModalActions.Clean());
+    try {
+      const data = await SellReportsService.byItems(filters);
+      if (!data) {
+        dispatch(fail(failMessage));
+        dispatch(ModalActions.Error({ title: 'Error', body: failMessage }));
+        return;
+      }
+      dispatch(success(data));
+    } catch (err) {
+      dispatch(fail(err.message));
+      dispatch(LoginActions.Logout());
+      window.location.reload();
+    }
+  };
+};
+
 const SellsReportsActions = {
   byClients,
+  byItems,
 };
 
 export default SellsReportsActions;
