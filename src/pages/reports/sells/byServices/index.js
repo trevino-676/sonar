20d3e9aee4f8b5cbf5c553a3d9bbf2d49823169b
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-06 17:06:12 -0500

    Se creo la vista para el reporte de ventas por servicio

---
+++ src/pages/reports/sells/byServices/index.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import { useSelector, useDispatch } from 'react-redux';
+
+import SellsReportsActions from '../../../../actions/SellsReport.action';
+import FilterBar from '../../../../components/FiltersBar';
+import ReportTable from '../../../../components/ReportTable';
+import useDataColumns from './DataColumns';
+import useFilterForm from './FilterForm';
+
+import '../../../../styles/reports.css';
+
+const SellsByServices = () => {
+  const dispatch = useDispatch();
+  const dataReport = useSelector((state) => state.sell_reports.by_services);
+  const currencyFormatter = (cell) => (
+    <span>$ {parseFloat(cell).toFixed(2)}</span>
+  );
+  const [columns] = useDataColumns(currencyFormatter);
+  const [filters, handleChangeFilter, formFields] = useFilterForm();
+  const onSubmit = (filter) => dispatch(SellsReportsActions.byServices(filter));
+
+  useEffect(() => {
+    document.title = 'Reportes | Ventas por servicios';
+  }, []);
+
+  return (
+    <Container>
+      <FilterBar
+        dataFields={formFields}
+        onHandleChange={handleChangeFilter}
+        onHandleClick={() => onSubmit(filters)}
+      />
+      <div className="report-area">
+        {dataReport && (
+          <ReportTable
+            tableData={dataReport}
+            tableColumns={columns}
+            dataKey="uuid"
+          />
+        )}
+      </div>
+    </Container>
+  );
+};
+
+export default SellsByServices;
