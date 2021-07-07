6653a92c1fa97761ec659f6ce9be55a8dc07f70b
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-06 21:44:31 -0500

    Se crearon las rutas del reporte de ventas por servicios

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -3,6 +3,10 @@ import React from 'react';
 import Settings from '../pages/Settings';
 import CompanyPage from '../pages/companies';
 import UsersPage from '../pages/users';
+import SellsByClient from '../pages/reports/sells';
+import SellsByItems from '../pages/reports/sells/byItems/ReportByItems';
+import TaxablePerceptions from '../pages/reports/payroll/taxablePerceptions';
+import SellsByServices from '../pages/reports/sells/byServices';
 
 export default {
   Settings: {
@@ -16,5 +20,21 @@ export default {
   Users: {
     component: <UsersPage />,
     path: '/users',
+  },
+  SellsReport: {
+    path: '/reports/sells/by_client',
+    component: <SellsByClient />,
+  },
+  SellsByItem: {
+    path: '/reports/sells/by_items',
+    component: <SellsByItems />,
+  },
+  SellsByServices: {
+    path: '/reports/sells/by_services',
+    component: <SellsByServices />,
+  },
+  TaxablesPerceptions: {
+    path: '/reports/payroll/taxables_perceptions',
+    component: <TaxablePerceptions />,
   },
 };
--- src/routes/PublicRoutes.js
+++ src/routes/PublicRoutes.js
@@ -1,10 +1,6 @@
 import Login from '../pages/Login';
 import Home from '../pages/Home';
 import Registro from '../pages/Registro';
-import SellsByClient from '../pages/reports/sells';
-import SellsByItems from '../pages/reports/sells/byItems/ReportByItems';
-import TaxablePerceptions from '../pages/reports/payroll/taxablePerceptions';
-import SellsByServices from '../pages/reports/sells/byServices';
 
 export default {
   Login: {
@@ -18,21 +14,5 @@ export default {
   UserRegistry: {
     component: Registro,
     path: '/registry',
-  },
-  SellsReport: {
-    path: '/reports/sells/by_client',
-    component: SellsByClient,
-  },
-  SellsByItem: {
-    path: '/reports/sells/by_items',
-    component: SellsByItems,
-  },
-  SellsByServices: {
-    path: '/reports/sells/by_services',
-    component: SellsByServices,
-  },
-  TaxablesPerceptions: {
-    path: '/report/payroll/taxables_perceptions',
-    component: TaxablePerceptions,
   },
 };
