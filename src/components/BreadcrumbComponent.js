c78735adaefa2248a0505d194fb38fdcb2fcd92d
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-07 21:07:39 -0500

    Se cambio el componente del breadcrumb y se agregaron estilos nuevos

--- src/components/BreadcrumbComponent.js
+++ src/components/BreadcrumbComponent.js
@@ -1,15 +1,21 @@
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
-import { Breadcrumb } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import '../styles/components/breadcrumb.css';
 
 const BreadcrumbComponent = ({ routes }) => (
-  <Breadcrumb>
-    {routes.map((route, index) => (
-      <Breadcrumb.Item href={route.path} active={route.active} key={index}>
-        {route.name}
-      </Breadcrumb.Item>
-    ))}
-  </Breadcrumb>
+  <>
+    <ul className="breadcrumb">
+      {routes.map((item, index) => (
+        <li>
+          <Link to={item.path} key={`B-${index}`}>
+            {item.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </>
 );
 
 export default BreadcrumbComponent;
