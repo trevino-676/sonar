0961996c9239e33cee088b85422b4d080249d9b5
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-15 14:15:11 -0500

    Se agrego el servicio para el reporte de retenciones

---
+++ src/service/reports/RetentionReport.service.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const BaseURL = 'http://localhost:5000';
+// const BaseURL = 'https://www.sonar32.com.mx';
+
+const retentionReport = async (filters) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `jwt ${localStorage.getItem('token')}`,
+  };
+
+  try {
+    const resp = await axios.get(`${BaseURL}/v1/retentions/reports/`, {
+      headers,
+      params: filters,
+    });
+    if (resp.status !== 200) return null;
+    return resp.data.data;
+  } catch (err) {
+    return null;
+  }
+};
+
+const RetentionsService = {
+  retentionReport,
+};
+
+export default RetentionsService;
