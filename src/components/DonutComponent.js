20ed92bc705dc1d3b4fc2d92bb34e561d7342993
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-28 14:38:32 -0500

    Se creo el componente de graficas de tipo dona

---
+++ src/components/DonutComponent.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Chart } from 'react-google-charts';
+
+import '../styles/components/DonutComponent.css';
+
+const DonutComponent = ({ top, data, title, route }) => (
+  <div className="donut-component">
+    <div className="donut-title">
+      <h4>{title}</h4>
+    </div>
+    <div className="donut-top">
+      {top.map((item) => (
+        <span>{item}</span>
+      ))}
+    </div>
+    <div className="donut-graph" id="donut-chart">
+      <Chart
+        width="19rem"
+        height="15.18rem"
+        chartType="PieChart"
+        loader={<div>Cargando</div>}
+        data={data}
+        options={{ pieHole: 0.4 }}
+      />
+    </div>
+    <div className="donut-detail">
+      <Link to={route}>Ver detalles</Link>
+    </div>
+  </div>
+);
+
+export default DonutComponent;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
+import DonutComponent from '../components/DonutComponent';
 
-const Home = () => (
+const Home = () => {
+  const title = 'Ventas por cliente';
+  const top = ['Cliente 1', 'Cliente 2', 'Cliente 3', 'Cliente 4'];
+  const data = [
+    ['Clientes', 'Ventas'],
+    ['Cliente 1', 200000.0],
+    ['Cliente 2', 100000.0],
+    ['Cliente 3', 50000.0],
+    ['Cliente 4', 150000.0],
+  ];
+
+  return (
     <>
-    <h1>Home</h1>
+      <DonutComponent
+        top={top}
+        data={data}
+        title={title}
+        route="/reports/sells/by_client"
+      />
     </>
-)
+  );
+};
 
 export default Home;
