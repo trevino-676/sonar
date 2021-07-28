2de0db98c58b17a492274bd1ebe8185a53a76a5a
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-27 23:14:58 -0500

    se agrego el ordenamiento de las columnas en el reporte de percepciones gravables

--- src/pages/reports/payroll/taxablePerceptions/DatatableColumns.js
+++ src/pages/reports/payroll/taxablePerceptions/DatatableColumns.js
@@ -3,28 +3,33 @@ const useDataColumns = (formatter) => {
     {
       dataField: '_id.empleado',
       text: 'Empleado',
+      sort: true,
     },
     {
       dataField: '_id.rfc',
       text: 'RFC',
+      sort: true,
     },
     {
       dataField: 'total_grabado',
       text: 'Gravado',
       align: 'right',
       formatter,
+      sort: true,
     },
     {
       dataField: 'total_exento',
       text: 'Exento',
       align: 'right',
       formatter,
+      sort: true,
     },
     {
       dataField: 'total_sueldo',
       text: 'Sueldo',
       align: 'right',
       formatter,
+      sort: true,
     },
   ];
 
