f5ff607a4d3c6f370cd04904e1301b6d3d82fcd0
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-07-27 22:34:32 -0500

    Se agrego el estilo de activo en la configuracion

--- src/components/ConfigurationLayout.js
+++ src/components/ConfigurationLayout.js
@@ -14,6 +14,42 @@ const ConfigurationLayout = () => {
     scheluder: true,
   });
 
+  const _changeActive = (id) => {
+    const company = document.getElementById('companies');
+    const dashboard = document.getElementById('dashboard');
+    const notification = document.getElementById('notification');
+    const scheduler = document.getElementById('scheduler');
+
+    switch (id) {
+      case 'company':
+        company.classList.add('active');
+        dashboard.classList.remove('active');
+        notification.classList.remove('active');
+        scheduler.classList.remove('active');
+        break;
+      case 'dashboard':
+        company.classList.remove('active');
+        dashboard.classList.add('active');
+        notification.classList.remove('active');
+        scheduler.classList.remove('active');
+        break;
+      case 'notification':
+        company.classList.remove('active');
+        dashboard.classList.remove('active');
+        notification.classList.add('active');
+        scheduler.classList.remove('active');
+        break;
+      case 'scheduler':
+        company.classList.remove('active');
+        dashboard.classList.remove('active');
+        notification.classList.remove('active');
+        scheduler.classList.add('active');
+        break;
+      default:
+        break;
+    }
+  };
+
   const handleClick = (event) => {
     const initialElements = {
       companies: true,
@@ -27,12 +63,28 @@ const ConfigurationLayout = () => {
           ...initialElements,
           companies: false,
         });
+        _changeActive('company');
         break;
       case 'Dashboard':
         setHiddenElements({
           ...initialElements,
           dashboard: false,
         });
+        _changeActive('dashboard');
+        break;
+      case 'Notificaciones':
+        setHiddenElements({
+          ...initialElements,
+          notification: false,
+        });
+        _changeActive('notification');
+        break;
+      case 'Scheduler':
+        setHiddenElements({
+          ...initialElements,
+          scheluder: false,
+        });
+        _changeActive('scheduler');
         break;
       default:
         setHiddenElements({
@@ -47,16 +99,32 @@ const ConfigurationLayout = () => {
       <h1>Configuración</h1>
       <div className="config-group">
         <div className="config-aside item">
-          <div className="config-aside-item" onClick={handleClick}>
+          <div
+            className="config-aside-item active"
+            id="companies"
+            onClick={handleClick}
+          >
             <span className="text-center">Mis empresas</span>
           </div>
-          <div className="config-aside-item" onClick={handleClick}>
+          <div
+            className="config-aside-item"
+            id="dashboard"
+            onClick={handleClick}
+          >
             <span className="text-center">Dashboard</span>
           </div>
-          <div className="config-aside-item" onClick={handleClick}>
+          <div
+            className="config-aside-item"
+            onClick={handleClick}
+            id="notification"
+          >
             <span className="text-center">Notificaciones</span>
           </div>
-          <div className="config-aside-item" onClick={handleClick}>
+          <div
+            className="config-aside-item"
+            onClick={handleClick}
+            id="scheduler"
+          >
             <span className="text-center">Scheduler</span>
           </div>
         </div>
@@ -66,6 +134,12 @@ const ConfigurationLayout = () => {
           </div>
           <div hidden={hiddenElements.dashboard}>
             <h1>dashboard</h1>
+          </div>
+          <div hidden={hiddenElements.notification}>
+            <h1>Notificaciones</h1>
+          </div>
+          <div hidden={hiddenElements.scheluder}>
+            <h1>Scheduler</h1>
           </div>
         </div>
       </div>
