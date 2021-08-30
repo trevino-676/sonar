383eb7b37b127dffc8ff8da9b46f4b7389aeac2e
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-08-30 11:53:16 -0500

    Se agrego el titulo de la pagina de configuracion de notificaciones

--- src/constants/system.constants.js
+++ src/constants/system.constants.js
@@ -2,6 +2,8 @@ const SystemConstants = {
   MAIN_LOGO: 'https://sonar32.s3.us-east-2.amazonaws.com/main_logo.png',
   NOTIFICATION_LOGO:
     'https://sonar32.s3.us-east-2.amazonaws.com/ci%CC%81rculo+azul.png',
+  // Page Titles
+  CONFIGURATION_NOTIFICATION: 'sonar | notificaciones',
 };
 
 export default SystemConstants;
