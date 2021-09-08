7e96d259cf1f852eaa765a70bfc5a1a668100a0b
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-09-08 17:10:16 -0500

    wizard primera parte.

--- src/pages/config/wizard.js
+++ src/pages/config/wizard.js
@@ -1,29 +1,43 @@
 import React, { useState } from 'react';
-import { Tabs, Tab, ProgressBar } from 'react-bootstrap';
+import { Tabs, Tab, ProgressBar, Modal, Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+
+import ModalActions from '../../actions/modal.action';
+import UserRegistry from '../users/register';
+import CompanyPage from '../companies';
 
 const RegistryWizard = () => {
+  const dispatch = useDispatch();
   const [key, setKey] = useState('registry');
-
+  const closeModal = () => dispatch(ModalActions.Clean());
   return (
-    <Tabs
-      id="controlled-tab-wizard"
-      activeKey={key}
-      onSelect={(k) => setKey(k)}
-      className="mb-3"
-    >
-      <Tab eventKey="registry" title="Registro">
-        <h1>Registro</h1>
-      </Tab>
-      <Tab eventKey="company" title="Empresas">
-        <h1>Empresas</h1>
-      </Tab>
-      <Tab eventKey="notification" title="Notificaciones">
-        <h1>Notificaciones</h1>
-      </Tab>
-      <Tab eventKey="scheduler" title="Schedulers">
-        <h1>Scheduler</h1>
-      </Tab>
-    </Tabs>
+    <>
+      <Tabs
+        id="controlled-tab-wizard"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="mb-3"
+      >
+        <Tab eventKey="registry" title="Registro">
+          <UserRegistry />
+        </Tab>
+        <Tab eventKey="company" title="Empresas">
+          {/* <CompanyPage /> */}
+          <h2>Empresas</h2>
+        </Tab>
+        <Tab eventKey="notification" title="Notificaciones">
+          <h1>Notificaciones</h1>
+        </Tab>
+        <Tab eventKey="scheduler" title="Schedulers">
+          <h1>Scheduler</h1>
+        </Tab>
+      </Tabs>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={closeModal}>
+          Cerrar
+        </Button>
+      </Modal.Footer>
+    </>
   );
 };
 
