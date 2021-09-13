e06f08b889ab52a282e2f1e724644f4b61a3c4c8
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-09-13 11:33:48 -0500

    Se agrego la configuracion del dashboard en el wizzard

--- src/pages/config/scheduler/rpa.js
+++ src/pages/config/scheduler/rpa.js
@@ -4,7 +4,6 @@ import { useDispatch } from 'react-redux';
 import ConfigActions from '../../../actions/config.action';
 import useReportTitle from '../../../hooks/useReportTitle';
 import SystemConstants from '../../../constants/system.constants';
-import ConfigConstants from '../../../constants/config.constants';
 
 import '../../../styles/pages/config/scheduler.css';
 
@@ -12,76 +11,65 @@ const RPAScheduler = ({ config }) => {
   useReportTitle(SystemConstants.CONFIGURATION_RPA);
   const dispatch = useDispatch();
   const [rpa, setRPA] = useState(config.rpa);
-  const[opinion, setOpinion] = useState(config.opinion);
-  const [cfdis,setCfdis] =useState(config.cfdis);
+  const [opinion, setOpinion] = useState(config.opinion);
+  const [cfdis, setCfdis] = useState(config.cfdis);
   const onChangeOpinion = (event) => {
-    const date = new Date(event.target.value + "T00:00:00");
+    const date = new Date(`${event.target.value}T00:00:00`);
     setRPA(date.getDay());
   };
   const onChange = (event) => {
-    const date = new Date(event.target.value + "T00:00:00");
+    const date = new Date(`${event.target.value}T00:00:00`);
     setOpinion(date.getDay());
   };
   const onChangeCfdis = (event) => {
-    const date = new Date(event.target.value + "T00:00:00");
+    const date = new Date(`${event.target.value}T00:00:00`);
     setCfdis(date.getDay());
   };
   useEffect(() => {
     setRPA(config.rpa);
     setOpinion(config.opinion);
-    setCfdis(config.cfdis)
+    setCfdis(config.cfdis);
   }, []);
   useEffect(() => {
     if (rpa !== config.rpa) {
       const data = { ...config, rpa };
       dispatch(ConfigActions.updateUSerConfig(data));
     }
-    if (opinion !== config.opinion){
-      const data = { ...config, opinion};
+    if (opinion !== config.opinion) {
+      const data = { ...config, opinion };
       dispatch(ConfigActions.updateUSerConfig(data));
     }
-    if (cfdis !== config.cfdis){
-      const data = { ...config, cfdis};
+    if (cfdis !== config.cfdis) {
+      const data = { ...config, cfdis };
       dispatch(ConfigActions.updateUSerConfig(data));
     }
-  }, [rpa,opinion,cfdis]);
+  }, [rpa, opinion, cfdis]);
 
   return (
     <>
-      <div class="container">
-        <div class="row">
-          <div class="col">
+      <div className="container">
+        <div className="row">
+          <div className="col">
             <p>Escoge cada cuando consultar EFOS</p>
             <div className="schedule">
-              <input 
-                type="date"
-                onChange={onChange}>
-              </input>
+              <input type="date" onChange={onChange} />
             </div>
           </div>
-          <div class="col">
+          <div className="col">
             <p>Escoge cada cuando descargar la Opinion</p>
             <div className="schedule">
-              <input
-              type="date"
-              onChange={onChangeOpinion}>
-              </input>
+              <input type="date" onChange={onChangeOpinion} />
             </div>
           </div>
-          <div class="col">
+          <div className="col">
             <p>Escoge cada cuando descargar CFDIs</p>
             <div className="schedule">
-              <input
-              type="date"
-              onChange={onChangeCfdis}>
-              </input>
+              <input type="date" onChange={onChangeCfdis} />
             </div>
           </div>
         </div>
       </div>
     </>
-
-
   );
 };
 
