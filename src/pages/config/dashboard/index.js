cae231f1b73eba493252d4785ff155d4c3a0d4c0
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-09-13 11:33:48 -0500

    Se agrego la configuracion del dashboard en el wizzard

--- src/pages/config/dashboard/index.js
+++ src/pages/config/dashboard/index.js
@@ -5,16 +5,14 @@ import HomeConfig from './HomeTab';
 
 import '../../../styles/pages/config/notifications.css';
 
-const DeshboardsConfigComponent = ({ config }) => {
-  return (
-    <>
-      <Tabs defaultActiveKey="dashboard" className="mb-3">
-        <Tab eventKey="dashboard" title="Dashboard">
-          <HomeConfig config={config} />
-        </Tab>
-      </Tabs>
-    </>
-  );
-};
+const DeshboardsConfigComponent = ({ config }) => (
+  <>
+    <Tabs defaultActiveKey="dashboard" className="mb-3">
+      <Tab eventKey="dashboard" title="Dashboard">
+        <HomeConfig config={config} />
+      </Tab>
+    </Tabs>
+  </>
+);
 
 export default DeshboardsConfigComponent;
