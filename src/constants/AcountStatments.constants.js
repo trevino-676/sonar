ea21af3367305e3ea06d40ce147cedc7958a5beb
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-10-06 14:42:02 -0500

    Se crearon las constantes para los estados de cuentas

---
+++ src/constants/AcountStatments.constants.js
@@ -0,0 +1,22 @@
+const AccountStatmentsConstants = {
+  PAGE_TITLE: 'Sonar | Estados de cuenta',
+  UPLOAD_ACCOUNT_STATMENT_SUCCESS_MESSAGE:
+    'Se cargo correctamente el estado de cuenta',
+  UPLOAD_ACCOUNT_STATMENT_ERROR_MESSAGE:
+    'Hubo un error al cargar el estado de cuenta',
+  BANKS: {
+    SANTANDER: 'santander',
+    HSBC: 'hsbc',
+    BBVA: 'bbva',
+    CITIBANAMEX: 'citibanamex',
+    BANORTE: 'banorte',
+    BANREGIO: 'banregio',
+    AFIRMA: 'afirma',
+    SCOTIABANK: 'scotiabank',
+  },
+  UPLOAD_ACCCOUNT_STATMENTS_REQUEST: 'UPLOAD_ACCCOUNT_STATMENTS_REQUEST',
+  UPLOAD_ACCCOUNT_STATMENTS_SUCCESS: 'UPLOAD_ACCCOUNT_STATMENTS_SUCCESS',
+  UPLOAD_ACCCOUNT_STATMENTS_FAIL: 'UPLOAD_ACCCOUNT_STATMENTS_FAIL',
+};
+
+export default AccountStatmentsConstants;
