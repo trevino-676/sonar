d0b5ee66c93187cf2ef1317cd81ee96271c1110e
Luis Manuel Torres Trevino <luis.trevino@drumbot.com.mx>
2021-10-06 20:43:49 -0500

    Se creo el servicio de la carga de estados de cuenta.

---
+++ src/service/account_statments/upload_account_service.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const BaseURL = 'http://localhost:8000';
+// const BaseURL = 'https://api.sonar32.com.mx';
+
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `jwt ${localStorage.getItem('token')}`,
+};
+
+const uploadAccountStatments = async (bank, file) => {
+  const formData = new FormData();
+  formData.append('file', file);
+  try {
+    const resp = await axios.post(`${BaseURL}/v1/upload/${bank}`, formData, {
+      headers,
+    });
+    if (resp.status !== 200) {
+      return null;
+    }
+    return resp.data.status;
+  } catch (e) {
+    return null;
+  }
+};
+
+const uploadAccountStatmentsService = {
+  uploadAccountStatments,
+};
+
+export default uploadAccountStatmentsService;
